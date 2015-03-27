b7f1239d59ba5f281384811e52be554ae8712c43
Wei Shi <wei.shi029@gmail.com>
2015-03-28 09:13:03 +1300

    Finished generateRandomString function

--- 01ContentSlider/Quiz/js/script.js
+++ 01ContentSlider/Quiz/js/script.js
@@ -3,22 +3,30 @@
 //This is Jun's task: write a javascript function to get length of a string 
 
 function getStringLen(str) {
-
+	
 }
 
 
 
 //This is Wei's task: generate a random string with given length
 
-function generateRandomString(length) {
+function generateRandomString(strlength) {
+	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	var result = '';
+	for(i = 0; i < strLength; i++) {
+		var index = Math.floor(Math.random() * strLength) + 1;
+		result += alphabet[index];
+	};
 
+	return result;
+	
 } 
 
 
 //Run
 var testString = 'Hello';
 var strLength = getStringLen(testString);
-var resultString = generateRandomString(testString);
+var resultString = generateRandomString(strLength);
 
 console.log('test string: ' + testString);
 console.log('result string: ' + resultString);
