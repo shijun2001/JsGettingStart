f3e43cb15dbf8c8804e29a7112c84fef79057aad
Wei Shi <wei.shi029@gmail.com>
2015-03-28 09:01:52 +1300

    Added tasks for generating random string

---
+++ 01ContentSlider/Quiz/js/script.js
@@ -0,0 +1,24 @@
+// let us work together
+
+//This is Jun's task: write a javascript function to get length of a string 
+
+function getStringLen(str) {
+
+}
+
+
+
+//This is Wei's task: generate a random string with given length
+
+function generateRandomString(length) {
+
+} 
+
+
+//Run
+var testString = 'Hello';
+var strLength = getStringLen(testString);
+var resultString = generateRandomString(testString);
+
+console.log('test string: ' + testString);
+console.log('result string: ' + resultString);
