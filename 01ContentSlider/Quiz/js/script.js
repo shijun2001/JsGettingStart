ac2e151bcb1007c42686b8a7ac4aca317f83454a
shijun2001 <jun.shi82@gmail.com>
2015-03-28 18:59:35 +0900

    var index = ...26

--- 01ContentSlider/Quiz/js/script.js
+++ 01ContentSlider/Quiz/js/script.js
@@ -15,8 +15,8 @@ function generateRandomString(strlength) {
 	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 	var result = '';
 	for(i = 0; i < strLength; i++) {
-		var index = Math.floor(Math.random() * strLength) + 1;
-		result += alphabet[index % 26];
+		var index = Math.floor(Math.random() * 26);
+		result += alphabet[index];
 	};
 
 	return result;
